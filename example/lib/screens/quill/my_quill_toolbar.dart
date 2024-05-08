a487cc9e8188a52397c51a7fab5aca3cf8e39f95
yimsb <yimsb000626@naver.com>
2024-05-08 16:25:30 +0900

    20240508samsung

--- example/lib/screens/quill/my_quill_toolbar.dart
+++ example/lib/screens/quill/my_quill_toolbar.dart
@@ -115,50 +115,50 @@ class MyQuillToolbar extends StatelessWidget {
                       Icons.width_normal,
                     ),
                   ),
-                  QuillToolbarHistoryButton(
-                    isUndo: true,
-                    controller: controller,
-                  ),
-                  QuillToolbarHistoryButton(
-                    isUndo: false,
-                    controller: controller,
-                  ),
-                  QuillToolbarToggleStyleButton(
-                    options: const QuillToolbarToggleStyleButtonOptions(),
-                    controller: controller,
-                    attribute: Attribute.bold,
-                  ),
-                  QuillToolbarToggleStyleButton(
-                    options: const QuillToolbarToggleStyleButtonOptions(),
-                    controller: controller,
-                    attribute: Attribute.italic,
-                  ),
-                  QuillToolbarToggleStyleButton(
-                    controller: controller,
-                    attribute: Attribute.underline,
-                  ),
-                  QuillToolbarClearFormatButton(
-                    controller: controller,
-                  ),
-                  const VerticalDivider(),
-                  QuillToolbarImageButton(
-                    controller: controller,
-                  ),
-                  QuillToolbarCameraButton(
-                    controller: controller,
-                  ),
-                  QuillToolbarVideoButton(
-                    controller: controller,
-                  ),
-                  const VerticalDivider(),
-                  QuillToolbarColorButton(
-                    controller: controller,
-                    isBackground: false,
-                  ),
-                  QuillToolbarColorButton(
-                    controller: controller,
-                    isBackground: true,
-                  ),
+                  // QuillToolbarHistoryButton(
+                  //   isUndo: true,
+                  //   controller: controller,
+                  // ),
+                  // QuillToolbarHistoryButton(
+                  //   isUndo: false,
+                  //   controller: controller,
+                  // ),
+                  // QuillToolbarToggleStyleButton(
+                  //   options: const QuillToolbarToggleStyleButtonOptions(),
+                  //   controller: controller,
+                  //   attribute: Attribute.bold,
+                  // ),
+                  // QuillToolbarToggleStyleButton(
+                  //   options: const QuillToolbarToggleStyleButtonOptions(),
+                  //   controller: controller,
+                  //   attribute: Attribute.italic,
+                  // ),
+                  // QuillToolbarToggleStyleButton(
+                  //   controller: controller,
+                  //   attribute: Attribute.underline,
+                  // ),
+                  // QuillToolbarClearFormatButton(
+                  //   controller: controller,
+                  // ),
+                  // const VerticalDivider(),
+                  // QuillToolbarImageButton(
+                  //   controller: controller,
+                  // ),
+                  // QuillToolbarCameraButton(
+                  //   controller: controller,
+                  // ),
+                  // // QuillToolbarVideoButton(
+                  // //   controller: controller,
+                  // // ),
+                  // const VerticalDivider(),
+                  // QuillToolbarColorButton(
+                  //   controller: controller,
+                  //   isBackground: false,
+                  // ),
+                  // QuillToolbarColorButton(
+                  //   controller: controller,
+                  //   isBackground: true,
+                  // ),
                   const VerticalDivider(),
                   QuillToolbarSelectHeaderStyleDropdownButton(
                     controller: controller,
